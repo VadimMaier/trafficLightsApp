d4192075cf3f9d5ec42180b691abd1937da38409
Вадим <vadim_ponomarev_1989@mail.ru>
2021-01-26 08:17:51 -0300

    Initial Commit

---
+++ trafficLightsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLightsApp
+//
+//  Created by Вадим on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
